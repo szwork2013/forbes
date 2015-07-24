86acc431d458bbde6a2c55110f2c510ad8de6a2e
hldzlk <zhoulink90@gmail.com>
2015-07-24 11:30:11 +0800

    更新

--- app/scripts/controllers/article.js
+++ app/scripts/controllers/article.js
@@ -1,5 +1,5 @@
 angular.module('fbs.controllers')
-.controller('articleListCtrl',function($scope,Tools,DataAPI) {
+.controller('articleListCtrl',function($rootScope,$scope,Tools,DataAPI) {
 
     $scope.details = function (newsid) {
         Tools.pageSkip('article_single',{newsid : newsid});
@@ -62,67 +62,67 @@ angular.module('fbs.controllers')
   }
 
 
-  $http.get("http://" + location.host + "/serv/wxapi.ashx?action=getjsapiconfig&url="+location.href)
-    .success(function(wxapidata){
-            wx.config({
-                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: wxapidata.appId, // 必填，公众号的唯一标识
-                timestamp: wxapidata.timestamp, // 必填，生成签名的时间戳
-                nonceStr: wxapidata.nonceStr, // 必填，生成签名的随机串
-                signature:  wxapidata.signature,// 必填，签名，见附录1
-                jsApiList: [
-                    "onMenuShareTimeline",
-                    "onMenuShareAppMessage",
-                    "onMenuShareQQ"
-                ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
-    })
-    .error(function(){
-    });
+  // $http.get("http://" + location.host + "/serv/wxapi.ashx?action=getjsapiconfig&url="+location.href)
+  //   .success(function(wxapidata){
+  //           wx.config({
+  //               debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+  //               appId: wxapidata.appId, // 必填，公众号的唯一标识
+  //               timestamp: wxapidata.timestamp, // 必填，生成签名的时间戳
+  //               nonceStr: wxapidata.nonceStr, // 必填，生成签名的随机串
+  //               signature:  wxapidata.signature,// 必填，签名，见附录1
+  //               jsApiList: [
+  //                   "onMenuShareTimeline",
+  //                   "onMenuShareAppMessage",
+  //                   "onMenuShareQQ"
+  //               ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  //           });
+  //   })
+  //   .error(function(){
+  //   });
 
-    wx.ready(function(){
+  //   wx.ready(function(){
 
-        wx.onMenuShareTimeline({
-            title: title, // 分享标题
-            link: location.href, // 分享链接
-            imgUrl: imgurl, // 分享图标
-            success: function() {
-                // alert("朋友圈分享成功!!!")
-            },
-            cancel: function() {
-                // alert("取消朋友圈分享!!!")
-            }
-        });
-        wx.onMenuShareAppMessage({
-            title: title, // 分享标题
-            desc: digest, // 分享描述
-            link: location.href, // 分享链接
-            imgUrl: imgurl, // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function() {
-                // alert("好友分享成功!!!")
-                sharetype = 0;
-                $scope.bbb(articleId,sharetype);
-            },
-            cancel: function() {
-                // alert("取消好友分享!!!")
-            }
-        });
-        wx.onMenuShareQQ({
-            title: title, // 分享标题
-            desc: digest, // 分享描述
-            link: location.href, // 分享链接
-            imgUrl: imgUrl, // 分享图标
-            success: function() {
-                // alert("qq分享成功!!!")
-            },
-            cancel: function() {
-                // alert("取消qq分享!!!")
-            }
-        });
-    })
-   // weixin
+  //       wx.onMenuShareTimeline({
+  //           title: title, // 分享标题
+  //           link: location.href, // 分享链接
+  //           imgUrl: imgurl, // 分享图标
+  //           success: function() {
+  //               // alert("朋友圈分享成功!!!")
+  //           },
+  //           cancel: function() {
+  //               // alert("取消朋友圈分享!!!")
+  //           }
+  //       });
+  //       wx.onMenuShareAppMessage({
+  //           title: title, // 分享标题
+  //           desc: digest, // 分享描述
+  //           link: location.href, // 分享链接
+  //           imgUrl: imgurl, // 分享图标
+  //           type: '', // 分享类型,music、video或link，不填默认为link
+  //           dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+  //           success: function() {
+  //               // alert("好友分享成功!!!")
+  //               sharetype = 0;
+  //               $scope.bbb(articleId,sharetype);
+  //           },
+  //           cancel: function() {
+  //               // alert("取消好友分享!!!")
+  //           }
+  //       });
+  //       wx.onMenuShareQQ({
+  //           title: title, // 分享标题
+  //           desc: digest, // 分享描述
+  //           link: location.href, // 分享链接
+  //           imgUrl: imgUrl, // 分享图标
+  //           success: function() {
+  //               // alert("qq分享成功!!!")
+  //           },
+  //           cancel: function() {
+  //               // alert("取消qq分享!!!")
+  //           }
+  //       });
+  //   })
+  //  // weixin
 
 
 
